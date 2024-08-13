e34223a1784d61d8893572e7b14af79c73c69802
Ashley Thompson <workashleythompson@gmail.com>
2024-08-13 22:35:27 +1000

    Complete List Virtualization demo (react-virtualized)

--- packages/react/list-virtualisation/src/App.tsx
+++ packages/react/list-virtualisation/src/App.tsx
@@ -1,31 +1,56 @@
 import "./App.css";
-import { List, type ListRowProps } from "react-virtualized";
+import { AutoSizer, CellMeasurer, CellMeasurerCache, List, type ListRowProps } from "react-virtualized";
 
 import { generateList } from "./utils";
+import { useRef } from "react";
 
-// List Array containing data objects that will be rendered as a row
 const list = generateList({ rowCount: 20 });
 
-// Required to render each row in the list
-// TODO: Calculate the height of each row
-const renderRow = ({ index, key, style }: ListRowProps) => (
-  <div key={key} style={style}>
-    <div className="flex justify-between p-4 bg-neutral-900 rounded-md shadow-sm">
-      <div>
-        <p>{list[index].name}</p>
-        <p>{list[index].text}</p>
+const renderRow = ({ index, key, style, parent }: ListRowProps, cache: React.MutableRefObject<CellMeasurerCache>) => {
+  const author = list[index];
+
+  return (
+    <CellMeasurer key={key} cache={cache.current} parent={parent} columnIndex={0} rowIndex={index}>
+      <div style={style} className={[index === 0 ? "pt-4" : "", index !== list.length ? "pb-4" : ""].join(" ")}>
+        <div
+          className={"flex justify-between gap-4 p-4 rounded-md shadow-lg ".concat(
+            index % 2 === 0 || index === list.length - 1 ? "bg-neutral-700" : "bg-neutral-900"
+          )}
+        >
+          <img src={author.image} className="h-12 rounded-full flex-start min-w-12" alt={author.name} />
+
+          <div>
+            <p className="font-bold font-lg">{author.name}</p>
+            <p>{author.text}</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <img src={list[index].image} alt={list[index].name} height={48} width={48} />
-      </div>
-    </div>
-  </div>
-);
+    </CellMeasurer>
+  );
+};
 
 function App() {
+  const cache = useRef(
+    new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: 100,
+    })
+  );
+
   return (
-    <div>
-      <List rowHeight={200} rowCount={list.length} rowRenderer={renderRow} width={800} height={600} overscanRowCount={3} />
+    <div className="w-[800px] max-w-full min-h-screen">
+      <AutoSizer>
+        {({ height, width }) => (
+          <List
+            width={width}
+            height={height}
+            rowHeight={cache.current.rowHeight}
+            rowCount={list.length}
+            rowRenderer={(props) => renderRow(props, cache)}
+            deferredMeasurementCache={cache.current}
+          />
+        )}
+      </AutoSizer>
     </div>
   );
 }
