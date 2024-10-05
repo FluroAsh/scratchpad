2b856a1f66c52e23f29021a8fc20583fe69b8633
Ashley Thompson <workashleythompson@gmail.com>
2024-10-05 16:41:03 +1000

    Add first build-your-own-react exercise (create element function)

---
+++ packages/react/build-your-own-react/README.md
@@ -0,0 +1,3 @@
+### Source
+
+Inspired by [Rodrigo Pombo's implementation](https://pomb.us/build-your-own-react/).
---
+++ packages/react/build-your-own-react/src/index.js
@@ -0,0 +1,57 @@
+function createElement(type, props, ...children) {
+  console.log("Didact.createElement", { type, props, children });
+
+  return {
+    type,
+    props: {
+      ...props,
+      // handles children that are primitive values
+      children: children.map((child) => (typeof child === "object" ? child : createTextElement(child))),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function render(element, container) {
+  const node = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
+
+  const isProperty = (key) => key !== "children";
+
+  // if the key is not "children" we add it to the DOM (root container element)
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      node[name] = element.props[name];
+    });
+
+  element.props.children.forEach((child) => {
+    render(child, node);
+  });
+
+  container.appendChild(node);
+}
+
+const Didact = {
+  createElement,
+  render,
+};
+
+/** @jsx Didact.createElement */
+const element = (
+  <div className="container">
+    <h1>Hello World</h1>
+    <h2 style="text-align:right">from Didact</h2>
+  </div>
+);
+
+const container = document.getElementById("root");
+Didact.render(element, container);
