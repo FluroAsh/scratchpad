6daf619643d0b267744420928d366f6c8d0b7b7a
Ashley Thompson <workashleythompson@gmail.com>
2024-10-29 20:48:26 +1100

    feat: Add NX for monorpeo support

--- packages/testing/vitest-snapshot/vitest.config.ts
+++ packages/testing/vitest-snapshot/vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config.js';
 
 export default defineConfig({
   test: {
     browser: {
       enabled: true,
-      name: "chromium",
-      provider: "playwright",
-      screenshotDirectory: "snapshots",
+      name: 'chromium',
+      provider: 'playwright',
+      screenshotDirectory: 'snapshots',
       // https://playwright.dev
       providerOptions: {},
     },
