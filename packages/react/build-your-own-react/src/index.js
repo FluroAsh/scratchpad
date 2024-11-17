8cd0365f93c2cc2a106ddcdc4c69dccc8b19709d
Ashley Thompson <workashleythompson@gmail.com>
2024-11-17 22:06:16 +1100

    build-yo-react: Part 4 - Fibers

--- packages/react/build-your-own-react/src/index.js
+++ packages/react/build-your-own-react/src/index.js
@@ -21,23 +21,26 @@ function createTextElement(text) {
   };
 }
 
-function render(element, container) {
-  const node = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
+function createDom(fiber) {
+  const dom = fiber.type == "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(fiber.type);
 
   const isProperty = (key) => key !== "children";
-
-  // if the key is not "children" we add it to the DOM (root container element)
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      node[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
 
-  element.props.children.forEach((child) => {
-    render(child, node);
-  });
+  return dom;
+}
 
-  container.appendChild(node);
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
 }
 
 let nextUnitOfWork = null;
@@ -53,8 +56,61 @@ function workLoop(deadline) {
 }
 requestIdleCallback(workLoop);
 
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO
+function performUnitOfWork(fiber) {
+  // If the fiber has a dom property, create a DOM node for it
+  if (fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  // If the fiber has a parent, append the created DOM node to the parent's DOM node
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // Get the children elements from the fiber's props
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  // Iterate over the children elements to create fibers for each child
+  while (index < elements.length) {
+    const element = elements[index];
+
+    // Create a new fiber for the child element
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    // If this is the first child, set it as the child of the current fiber
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      // Otherwise, set it as the sibling of the previous child
+      prevSibling.sibling = newFiber;
+    }
+
+    // Update prevSibling to the current newFiber
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // If the fiber has a child, return the child to continue the work
+  if (fiber.child) {
+    return fiber.child; // start with child
+  }
+
+  let nextFiber = fiber;
+
+  // Traverse up the fiber tree to find the next sibling or parent
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling; // if no child we look for a sibling
+    }
+    nextFiber = nextFiber.parent; // if there is no child OR sibling, then we look for a parent
+  }
 }
 
 const Didact = {
