a5b69405abaa567326fa586330952227508088e0
Ashley Thompson <workashleythompson@gmail.com>
2024-11-17 22:26:49 +1100

    chore(tw): Init spotlight-card demo

---
+++ tailwind/spotlight-card/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen w-full items-center justify-center">
+      <h1 className="text-2xl font-bold">This is what dreaaaaams, are made of!</h1>
+      <p className="text-lg text-neutral-400">Let's get to work!</p>
+    </div>
+  );
+}
